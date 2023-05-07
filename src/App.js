e585e4c2eceae63080ffe74fc9c817e899826e3b
Jyoti Ranjan Biswal <jranjan2017@gmail.com>
2023-05-07 14:21:04 +0530

    notes - first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+import SaladMaker from './components/SaladMaker/SaladMaker';
+import UserContext from './components/User/User';
+
+const user = {
+  name: 'Jyoti',
+  favorites: [
+    'avocado',
+    'carrot'
+  ]
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <UserContext.Provider value={user}>
+        <Navigation />
+        <SaladMaker />
+      </UserContext.Provider>
+    </>
   );
 }
 
